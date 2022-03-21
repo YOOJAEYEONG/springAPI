ae30477ae83aa0c222f8981aba3d143c4b11cc86
YooJaeYeong <liujeayat@gmail.com>
2022-03-21 17:57:13 +0900

    devtool 제거 , log4j2-dev.yaml 구성이 적용 안되고있음.. 로그 두번씩 뜸.

--- src/main/java/com/spring/ref/web/TestRestController.java
+++ src/main/java/com/spring/ref/web/TestRestController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/testapi")
 public class TestRestController {
 
   final TestService testService;
